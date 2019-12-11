722f2786547ce4b1de55add91d07e9d1f38f7c06
wolel <56022602+wolel@users.noreply.github.com>
2019-12-11 12:26:38 +0100

    pierre papier

--- script_II_test.js
+++ script_II_test.js
@@ -1,8 +1,10 @@
 
 var score = document.getElementById('score');
-var txtScore = document.getElementById('txtScore');
+var txtScore = document.getElementById('scoreField');
 var btn = document.getElementById('btn');
-
+var imgPapier = document.getElementById('imgPapier');
+var imgPierre = document.getElementById('imgPierre');
+var imgSiceaux = document.getElementById('imgSiceaux');
 
 
 //----------determiner une variable pour le score-------------
@@ -31,84 +33,91 @@ for (var i = 0; i < imgV.length; i++) {
 
 
 //----------------------------------------------
-for (var i = 0; i < allImgPC.length;i++){
-
+for (var i = 0; i < allImgPC.length;i++) {
 
 
     imgV[i].addEventListener('click', function () {
-        var randomImagePC = Math.floor(Math.random()*allImgPC.length);
+        var randomImagePC = Math.floor(Math.random() * allImgPC.length);
         imgPC[randomImagePC].style.display = 'block';
 
-        if (randomImagePC === 0){
+        if (randomImagePC === 0) {
             imgPC[1].style.cssText = 'display:none';
             imgPC[2].style.cssText = 'display:none';
 
-        }
-        else if (randomImagePC === 1){
+        } else if (randomImagePC === 1) {
             imgPC[0].style.cssText = 'display:none';
             imgPC[2].style.cssText = 'display:none';
 
-        }
-        else if (randomImagePC === 2){
+        } else if (randomImagePC === 2) {
             imgPC[0].style.cssText = 'display:none';
             imgPC[1].style.cssText = 'display:none';
 
         }
         if ((choixUtilisateur === 'imgPapier' && randomImagePC === 0) ||
             (choixUtilisateur === 'imgPierre' && randomImagePC === 1) ||
-            (choixUtilisateur === 'imgSciseaux' && randomImagePC === 2))
-        {
+            (choixUtilisateur === 'imgSciseaux' && randomImagePC === 2)) {
 
 
             txtScore.value = 'EGALITE';
-        }
-        else if ((choixUtilisateur === 'imgPapier' && randomImagePC === 1) ||
+        } else if ((choixUtilisateur === 'imgPapier' && randomImagePC === 1) ||
             (choixUtilisateur === 'imgPierre' && randomImagePC === 2) ||
-            (choixUtilisateur === 'imgSciseaux' && randomImagePC === 1))
-        {
+            (choixUtilisateur === 'imgSciseaux' && randomImagePC === 1)) {
             countV++;
             txtScore.value = 'VICTOIRE !!';
 
-        }
-        else if ((choixUtilisateur === 'imgPapier' && randomImagePC === 2) ||
+        } else if ((choixUtilisateur === 'imgPapier' && randomImagePC === 2) ||
             (choixUtilisateur === 'imgPierre' && randomImagePC === 0) ||
-            (choixUtilisateur === 'imgSciseaux' && randomImagePC === 1))
-        {
+            (choixUtilisateur === 'imgSciseaux' && randomImagePC === 1)) {
             txtScore.value = 'PERDU';
             countF++
         }
-        if (countF === 3){
+        if (countF === 3) {
 
             txtScore.value = 'Le PC a gagner contre vous';
-            score.style.cssText ='display:none';
+            score.style.cssText = 'display:none';
             countF = 0;
             countV = 0;
             btn.style.display = 'inline';
+            imgPapier.style.display = 'none';
+            imgPierre.style.display = 'none';
+            imgSiceaux.style.display = 'none';
 
-        }else if (countV === 3){
+        } else if (countV === 3) {
 
-            txtScore.value ='Vous avez gagner contre le PC';
-            score.style.cssText ='display:none';
+            txtScore.value = 'Vous avez gagner contre le PC';
+            score.style.cssText = 'display:none';
             countF = 0;
             countV = 0;
             btn.style.display = 'inline';
-
+            //window.onclick = myFunction();
+            imgPapier.style.display = 'none';
+            imgPierre.style.display = 'none';
+            imgSiceaux.style.display = 'none';
         }
 
 
         score.value = countV + ' - ' + countF;
-        console.log(countV, countF);
+        //console.log(countV, countF);
         console.log(imgPC[randomImagePC]);
-
-
 
     });
 
-    //function rejouer(){
-      //  window.location.reload();}
+    //function rejouer() {window.location.reload();}
+
+
+    btn.addEventListener('click', function () {
+        window.location.reload();
+
+    });
+
 }
-//btn.addEventListener('click', function () {
-  //  window.location.reload();
+
+
+
+/*function myFunction() {
+    document.getElementById("col-sm-4").style.backgroundColor = 'yellow';
+    window.location.reload();
+}*/
 
 
 
